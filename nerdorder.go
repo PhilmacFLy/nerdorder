2f8cc94f25f06b739d5d16502468894ab4b0da7d
PhilmacFLy <PN@mtd-systems.de>
2015-01-16 01:14:42 +0100

    Erste design und code versuche

--- nerdorder.go
+++ nerdorder.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Frontpage struct {
+	lists []List
+}
+
+func statichandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/main.html")
+	t.Execute(w, nil)
+
+}
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/static/", statichandler)
+	http.ListenAndServe(":8080", nil)
+
+}
